8166d70517f515d971adfc53309ed47bdb8d7e25
exhonda <honda@ertl.jp>
2018-04-19 23:14:04 +0900

    add

---
+++ arch/arm_m_gcc/stm32l4xx_stm32cube/chip.tf
@@ -0,0 +1,8 @@
+$ 
+$     パス2のターゲット依存テンプレート（STM32L4XX_STM32CUBE用）
+$ 
+
+$ 
+$  コア依存のテンプレートファイルのインクルード
+$ 
+$INCLUDE"arm_m_gcc/common/core.tf"$
---
+++ target/dummy_gcc/target_cfg1_out.h
@@ -0,0 +1,12 @@
+/*
+ *  $Id: target_cfg1_out.h 2808 2017-05-16 03:37:57Z ertl-honda $
+ */
+
+/*
+ *		cfg1_out.cのリンクに必要なスタブの定義
+ */
+
+int main()
+{
+	return(0);
+}
